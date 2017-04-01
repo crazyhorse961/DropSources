26206b673279da8e851651d1360c00fc83597b91
BuildTools <ruben.oktay@gmail.com>
2017-04-01 13:01:06 +0200

    Modified event, added EventFactory in-progress

---
+++ src/main/java/com/crazyhoorse961/sourcedrops/events/EventFactory.java
@@ -0,0 +1,9 @@
+package com.crazyhoorse961.sourcedrops.events;
+
+/**
+ * Created by nini7 on 01.04.2017.
+ */
+public enum EventFactory
+{
+    
+}
--- src/main/java/com/crazyhoorse961/sourcedrops/events/PlayerOpenDropEvent.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/events/PlayerOpenDropEvent.java
@@ -1,21 +1,23 @@
-package com.crazyhoorse961.sourcedrops.events;/**
+package com.crazyhoorse961.sourcedrops.events; /**
  * Created by nini7 on 01.04.2017.
  */
 
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 /**
  * @author crazyhoorse961
  */
-public class PlayerOpenDropEvent extends Event
+public class PlayerOpenDropEvent extends Event implements Cancellable
 {
 
     private Player player;
     private Location dropLoc;
     private static final HandlerList HANDLERS = new HandlerList();
+    private boolean cancelled = false;
 
     @Override
     public HandlerList getHandlers() {
@@ -39,5 +41,13 @@ public class PlayerOpenDropEvent extends Event
 
     public Location getDropLocation() {
         return dropLoc;
+    }
+
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    public void setCancelled(boolean b) {
+        this.cancelled = b;
     }
 }
