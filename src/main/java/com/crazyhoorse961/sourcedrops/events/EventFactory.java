d74e5dd53d70d6b674eddf4a3a1b813703a53782
BuildTools <ruben.oktay@gmail.com>
2017-04-01 13:41:23 +0200

    Started EventFactory and created the open listener

--- src/main/java/com/crazyhoorse961/sourcedrops/events/EventFactory.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/events/EventFactory.java
@@ -1,9 +1,31 @@
 package com.crazyhoorse961.sourcedrops.events;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+
 /**
  * Created by nini7 on 01.04.2017.
  */
 public enum EventFactory
 {
-    
+    OPEN,SPAWN,REMOVE;
+
+    private Event event;
+
+    public void call(){
+
+    }
+    public EventFactory init(Player p, Location dropLoc){
+        switch(this){
+            case OPEN:
+                this.event = new PlayerOpenDropEvent(p, dropLoc);
+                return OPEN;
+            case SPAWN:
+                return SPAWN;
+            case REMOVE:
+                return REMOVE;
+        }
+        return null;
+    }
 }
---
+++ src/main/java/com/crazyhoorse961/sourcedrops/listeners/OpenListener.java
@@ -0,0 +1,12 @@
+package com.crazyhoorse961.sourcedrops.listeners;/**
+ * Created by nini7 on 01.04.2017.
+ */
+
+import org.bukkit.event.Listener;
+
+/**
+ * @author crazyhoorse961
+ */
+public class OpenListener implements Listener {
+
+}
