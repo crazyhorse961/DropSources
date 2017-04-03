920f4a98a1e31a9419149a129d713c42ac3dd7c0
BuildTools <ruben.oktay@gmail.com>
2017-04-03 17:45:43 +0200

    Updated itemserialized, addes commons io, created cache file method. Gotta run first test :)

--- src/main/java/com/crazyhoorse961/sourcedrops/SourceDrop.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/SourceDrop.java
@@ -3,8 +3,12 @@ package com.crazyhoorse961.sourcedrops;/**
  */
 
 import com.crazyhoorse961.sourcedrops.serializer.ChestSerializer;
+import com.crazyhoorse961.sourcedrops.serializer.ItemSerializer;
 import com.crazyhoorse961.sourcedrops.utils.Dumper;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author crazyhoorse961
@@ -14,11 +18,22 @@ public class SourceDrop extends JavaPlugin
 
     private Dumper dumper;
     private ChestSerializer chestSerializer;
+    private File cache;
+    private ItemSerializer itemSerializer;
 
     @Override
     public void onEnable(){
+        saveDefaultConfig();
+        createCache();
         dumper = new Dumper();
         chestSerializer = new ChestSerializer(this);
+        itemSerializer = new ItemSerializer(this);
+        System.out.println(itemSerializer);
+    }
+
+    @Override
+    public void onDisable(){
+        saveConfig();
     }
 
     public Dumper getDumper() {
@@ -27,5 +42,24 @@ public class SourceDrop extends JavaPlugin
 
     public ChestSerializer getChestSerializer() {
         return chestSerializer;
+    }
+
+    public File getCache() {
+        return cache;
+    }
+
+    public ItemSerializer getItemSerializer() {
+        return itemSerializer;
+    }
+
+    private void createCache() {
+        cache = new File(getDataFolder() + "cache.src");
+        if (!cache.exists()) {
+            try {
+                cache.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/com/crazyhoorse961/sourcedrops/serializer/ItemSerializer.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/serializer/ItemSerializer.java
@@ -2,16 +2,60 @@ package com.crazyhoorse961.sourcedrops.serializer;/**
  * Created by nini7 on 01.04.2017.
  */
 
+import com.crazyhoorse961.sourcedrops.SourceDrop;
+
+import com.google.common.base.Splitter;
+import org.apache.commons.io.FileUtils;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Base64;
+
+import java.io.*;
+
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * @author crazyhoorse961
  */
 public class ItemSerializer
 {
+    private final SourceDrop plugin;
+
+    public ItemSerializer(SourceDrop plugin){
+        this.plugin = plugin;
+    }
+
     public String serialize(ItemStack is){
+        Map<String,Object> values = is.serialize();
+        try {
+            PrintWriter pw = new PrintWriter(plugin.getDataFolder() + "cache.src");
+            pw.write("cache-item " + values);
+            pw.close();
+        }catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
+        return getCache();
+    }
+
+    public ItemStack deserialize(String des){
+        return null;
+    }
+
+    private String getCache(){
+        try {
+            byte[] encoded = Files.readAllBytes(Paths.get(plugin.getDataFolder() + "cache.src"));
+            List<String> lines = FileUtils.readLines(new File(plugin.getDataFolder() + "cache.src"), Charset.defaultCharset());
+            List<String> updatedLines = lines.stream().filter(s -> !s.contains("cache-item")).collect(Collectors.toList());
+            FileUtils.writeLines(new File(plugin.getDataFolder() + "cache.src"), updatedLines, false);
+            return String.valueOf(encoded).split(" ")[1];
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         return "";
     }
 }
