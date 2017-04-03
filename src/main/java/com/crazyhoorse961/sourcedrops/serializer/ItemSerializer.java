126a14fc485454a2792a462b86ce557c8dfa1b3d
BuildTools <ruben.oktay@gmail.com>
2017-04-03 18:30:18 +0200

    I'll reinvent the wheel another day...

--- src/main/java/com/crazyhoorse961/sourcedrops/serializer/ItemSerializer.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/serializer/ItemSerializer.java
@@ -4,19 +4,16 @@ package com.crazyhoorse961.sourcedrops.serializer;/**
 
 import com.crazyhoorse961.sourcedrops.SourceDrop;
 
-import com.google.common.base.Splitter;
-import org.apache.commons.io.FileUtils;
+import org.bukkit.Material;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-
+import javax.xml.bind.DatatypeConverter;
 import java.io.*;
-
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+
 
 /**
  * @author crazyhoorse961
@@ -29,34 +26,77 @@ public class ItemSerializer
         this.plugin = plugin;
     }
 
-    public String serialize(ItemStack is){
-        Map<String,Object> values = is.serialize();
+    public String itemsToString(ItemStack[] items) {
         try {
-            PrintWriter pw = new PrintWriter(plugin.getDataFolder() + File.separator + "cache.src");
-            pw.write("cache-item " + values);
-            pw.close();
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-        }
-
-        return getCache();
-    }
-
-    public ItemStack deserialize(String des){
-        return null;
-    }
-
-    private String getCache(){
-        try {
-            byte[] encoded = Files.readAllBytes(Paths.get(plugin.getDataFolder() + File.separator + "cache.src"));
-            System.out.println(encoded);
-            List<String> lines = FileUtils.readLines(new File(plugin.getDataFolder() + File.separator + "cache.src"), Charset.defaultCharset());
-            List<String> updatedLines = lines.stream().filter(s -> !s.contains("cache-item")).collect(Collectors.toList());
-            FileUtils.writeLines(new File(plugin.getDataFolder() + File.separator + "cache.src"), updatedLines, false);
-            return String.valueOf(encoded).split(" ")[1];
-        }catch(IOException e){
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            oos.writeObject(serializeItemStack(items));
+            oos.flush();
+            return DatatypeConverter.printBase64Binary(bos.toByteArray());
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return "";
+    }
+
+    @SuppressWarnings("unchecked")
+    public ItemStack[] stringToItems(String s) {
+        try {
+            ByteArrayInputStream bis = new ByteArrayInputStream(DatatypeConverter.parseBase64Binary(s));
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            return deserializeItemStack((Map<String, Object>[]) ois.readObject());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ItemStack[]{new ItemStack(Material.AIR)};
+    }
+
+    @SuppressWarnings("unchecked")
+    private Map<String, Object>[] serializeItemStack(ItemStack[] items) {
+
+        Map<String, Object>[] result = new Map[items.length];
+
+        for (int i = 0; i < items.length; i++) {
+            ItemStack is = items[i];
+            if (is == null) {
+                result[i] = new HashMap<>();
+            } else {
+                result[i] = is.serialize();
+                if (is.hasItemMeta()) {
+                    result[i].put("meta", is.getItemMeta().serialize());
+                }
+            }
+        }
+
+        return result;
+    }
+
+    @SuppressWarnings("unchecked")
+    private ItemStack[] deserializeItemStack(Map<String, Object>[] map) {
+        ItemStack[] items = new ItemStack[map.length];
+
+        for (int i = 0; i < items.length; i++) {
+            Map<String, Object> s = map[i];
+            if (s.size() == 0) {
+                items[i] = null;
+            } else {
+                try {
+                    if (s.containsKey("meta")) {
+                        Map<String, Object> im = new HashMap<>((Map<String, Object>) s.remove("meta"));
+                        im.put("==", "ItemMeta");
+                        ItemStack is = ItemStack.deserialize(s);
+                        is.setItemMeta((ItemMeta) ConfigurationSerialization.deserializeObject(im));
+                        items[i] = is;
+                    } else {
+                        items[i] = ItemStack.deserialize(s);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    items[i] = null;
+                }
+            }
+
+        }
+        return items;
     }
 }
