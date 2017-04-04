71b2ceb22acf2d6b0486066c204609e221e52fa3
BuildTools <ruben.oktay@gmail.com>
2017-04-04 18:04:28 +0200

    Modified dump command

--- src/main/java/com/crazyhoorse961/sourcedrops/commands/DumpCommand.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/commands/DumpCommand.java
@@ -2,8 +2,10 @@ package com.crazyhoorse961.sourcedrops.commands;/**
  * Created by nini7 on 01.04.2017.
  */
 
-import com.crazyhoorse961.sourcedrops.utils.Dumper;
+import com.crazyhoorse961.sourcedrops.SourceDrop;
+
 import net.md_5.bungee.api.ChatColor;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -15,9 +17,16 @@ import org.bukkit.command.ConsoleCommandSender;
 public class DumpCommand implements CommandExecutor
 {
 
+    private final SourceDrop plugin;
+
+    public DumpCommand(SourceDrop plugin){
+        this.plugin = plugin;
+        plugin.getCommand("dump").setExecutor(this);
+    }
+
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if(commandSender instanceof ConsoleCommandSender){
-            commandSender.sendMessage(ChatColor.GREEN + "Here you dump! " + new Dumper().generateHaste());
+            commandSender.sendMessage(ChatColor.GREEN + "Here you dump! " + plugin.getDumper().generateHaste());
             return true;
         }else{
             commandSender.sendMessage(ChatColor.RED + "You can dump only from the console");
