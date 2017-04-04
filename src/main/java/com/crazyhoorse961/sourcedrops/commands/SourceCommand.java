7ae919a6b692fd593fca76661d5d958324b445eb
BuildTools <ruben.oktay@gmail.com>
2017-04-04 19:08:38 +0200

    Starting adding the chest spawning method

--- src/main/java/com/crazyhoorse961/sourcedrops/commands/SourceCommand.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/commands/SourceCommand.java
@@ -2,9 +2,43 @@ package com.crazyhoorse961.sourcedrops.commands;/**
  * Created by nini7 on 01.04.2017.
  */
 
+import com.crazyhoorse961.sourcedrops.SourceDrop;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 /**
  * @author crazyhoorse961
  */
-public class SourceCommand {
+public class SourceCommand implements CommandExecutor
+{
 
+    private final SourceDrop plugin;
+
+    public SourceCommand(SourceDrop plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(!(commandSender instanceof Player)){
+            commandSender.sendMessage(ChatColor.RED + "You shall be a player to execute this command");
+            return true;
+        }
+        Player player = (Player) commandSender;
+        switch(strings.length){
+            case 0:
+                sendHelp(player);
+                return true;
+            case 1:
+                sendHelp(player);
+        }
+        return false;
+    }
+
+    private void sendHelp(Player p){
+
+    }
 }
---
+++ src/main/java/com/crazyhoorse961/sourcedrops/listeners/SpawnListener.java
@@ -0,0 +1,51 @@
+package com.crazyhoorse961.sourcedrops.listeners;/**
+ * Created by nini7 on 04.04.2017.
+ */
+
+import com.crazyhoorse961.sourcedrops.SourceDrop;
+import com.crazyhoorse961.sourcedrops.utils.HiddenStringUtil;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.material.MaterialData;
+import org.bukkit.metadata.FixedMetadataValue;
+
+/**
+ * @author crazyhoorse961
+ */
+public class SpawnListener implements Listener
+{
+
+    private final SourceDrop plugin;
+
+    public SpawnListener(SourceDrop plugin){
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onClick(PlayerInteractEvent e){
+        Player p = e.getPlayer();
+        if(e.getItem() != null && e.getItem().hasItemMeta() && e.getItem().getItemMeta().hasLore()){
+            String chestName = null;
+            for(String str : e.getItem().getItemMeta().getLore()){
+                if(HiddenStringUtil.hasHiddenString(str)){
+                    if(HiddenStringUtil.extractHiddenString(str).startsWith("chest ")){
+                        chestName = HiddenStringUtil.extractHiddenString(str).split(" ")[1];
+                        break;
+                    }
+                }
+            }
+            if(chestName.isEmpty()){
+                return;
+            }
+            Location newLoc = new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY() + 30, p.getLocation().getZ());
+            FallingBlock block = p.getWorld().spawnFallingBlock(newLoc, new MaterialData(Material.CHEST));
+            block.setMetadata(chestName, new FixedMetadataValue(plugin, chestName));
+            return;
+        }
+    }
+}
