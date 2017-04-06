b8d724f0c9ba7e4874962913e7bef8beb524e23f
BuildTools <ruben.oktay@gmail.com>
2017-04-06 17:53:15 +0200

    Modified config structure and worked on sourcecommand

--- src/main/java/com/crazyhoorse961/sourcedrops/commands/SourceCommand.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/commands/SourceCommand.java
@@ -4,10 +4,16 @@ package com.crazyhoorse961.sourcedrops.commands;/**
 
 import com.crazyhoorse961.sourcedrops.SourceDrop;
 import net.md_5.bungee.api.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author crazyhoorse961
@@ -37,14 +43,26 @@ public class SourceCommand implements CommandExecutor
             case 2:
                 switch(strings[0]){
                     case "create":
-
-                       // player.getTargetBlock(null, 10);
+                        if(!player.hasPermission("source.create")){
+                            player.sendMessage(ChatColor.RED + "Missing permission");
+                            return true;
+                        }
+                        Set<Material> empty = new HashSet<Material>();
+                        Block checkChest = player.getTargetBlock(empty, 10);
+                        if(checkChest.getType() == Material.CHEST){
+                            Chest chest = (Chest) checkChest.getState();
+                            plugin.getConfig().set("chests." + strings[1] + ".items", plugin.getItemSerializer().itemsToString(chest.getInventory().getContents()));
+                            plugin.getConfig().set("chests." + strings[1] + ".name", ChatColor.translateAlternateColorCodes('&', strings[1]));
+                            plugin.saveConfig();
+                            player.sendMessage(ChatColor.GREEN + "Chest created !");
+                        }
                     case "remove":
                         if(!player.hasPermission("source.remove")){
                             player.sendMessage(ChatColor.RED + "Missing permission");
                             return true;
                         }
                         plugin.getConfig().set("chests." + strings[1], null);
+                        plugin.saveConfig();
                         player.sendMessage(ChatColor.GREEN + "AirDrop removed!");
                         return true;
                 }
