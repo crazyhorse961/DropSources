2f10eb93569fd79f5ccb66a68e8a816cc0a14a64
BuildTools <ruben.oktay@gmail.com>
2017-04-21 11:12:08 +0200

    Worked on 'get' subcommand

--- src/main/java/com/crazyhoorse961/sourcedrops/commands/SourceCommand.java
+++ src/main/java/com/crazyhoorse961/sourcedrops/commands/SourceCommand.java
@@ -3,6 +3,7 @@ package com.crazyhoorse961.sourcedrops.commands;/**
  */
 
 import com.crazyhoorse961.sourcedrops.SourceDrop;
+import com.sun.istack.internal.Nullable;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -10,8 +11,13 @@ import org.bukkit.block.Chest;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -73,6 +79,8 @@ public class SourceCommand implements CommandExecutor {
                             player.sendMessage(ChatColor.RED + "Missing permission");
                             return true;
                         }
+                        giveKey(player, "testing", 1);
+
                 }
         }
         return false;
@@ -80,5 +88,21 @@ public class SourceCommand implements CommandExecutor {
 
     private void sendHelp(Player p) {
 
+    }
+
+    private void giveKey(Player p, String keyName, int amount){
+        int newAmount = amount == 0 ? 1 : amount;
+        ItemStack key = new ItemStack(Material.valueOf(plugin.getConfig().getString("keys-generic.material")), newAmount);
+        ItemMeta keyMeta = key.getItemMeta();
+        keyMeta.setDisplayName(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("keys-generic.name").replace("%KEY%", keyName).replace("%AMOUNT%", String.valueOf(newAmount))));
+        keyMeta.setLore(Arrays.asList(plugin.getConfig().getString("keys-generic.lore").replace("%KEY%", keyName).replace("%AMOUNT%", String.valueOf(newAmount)), "chest " + keyName));
+        if(plugin.getConfig().getBoolean("keys-generic.glowing")){
+            keyMeta.addEnchant(Enchantment.DEPTH_STRIDER, 1, true);
+            keyMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        }
+        key.setItemMeta(keyMeta);
+        p.getInventory().addItem(key);
+        p.sendMessage(ChatColor.GREEN + "You got " + newAmount + " keys!");
+        return;
     }
 }
